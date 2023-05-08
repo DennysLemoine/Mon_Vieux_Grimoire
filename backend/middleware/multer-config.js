dee05b59770495bcbaa12887388aadc991fc5236
YUHI <dennys.lemoine.1@gmail.com>
2023-05-08 10:37:19 +0200

    add sharp to project for better opti images

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
 
 const MIME_TYPES ={
     'images/jpg': 'jpg',
@@ -17,4 +19,32 @@ const storage = multer.diskStorage({
     }
 });
 
-module.exports = multer({storage}).single('image');
+const upload = multer({ storage }).single('image');
+
+// Middleware pour traiter l'image avant de l'enregistrer
+const processImage = (req, res, next) => {
+    if (!req.file) {
+        // Aucun fichier n'a été téléchargé
+        return next();
+    }
+
+    // Ouvrir l'image téléchargée avec sharp
+    sharp(req.file.path)
+        // Redimensionner l'image à 500 pixels de large
+        .resize({ width: 500 })
+        // Compresser l'image avec une qualité de 80%
+        .jpeg({ quality: 80 })
+        // Enregistrer l'image traitée dans un fichier
+        .toFile(req.file.path + '.optimized', (err) => {
+            if (err) {
+                return next(err);
+            }
+            // Supprimer l'ancien fichier et renommer le nouveau fichier
+            fs.unlinkSync(req.file.path);
+            req.file.filename = req.file.filename + '.optimized';
+            req.file.path = req.file.path + '.optimized';
+            next();
+        });
+};
+
+module.exports = { upload, processImage };
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config')
+const { upload, processImage } = require('../middleware/multer-config');
 
 const bookCtrl = require('../controllers/book');
 
 router.get('/',  bookCtrl.getAllBook);
 router.get('/:id', bookCtrl.getOneBook);
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.post('/', auth, upload, processImage, bookCtrl.createBook);
+router.put('/:id', auth, upload, processImage, bookCtrl.modifyBook);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
