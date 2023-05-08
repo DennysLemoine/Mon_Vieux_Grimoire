0f1d011b060615e5052f108d917f8e562e1997f7
YUHI <dennys.lemoine.1@gmail.com>
2023-05-08 11:50:53 +0200

    add rating notation and array of 3bestRatingsBooks

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -69,3 +69,83 @@ exports.deleteBook = (req, res, next) => {
         });
 };
 
+// Notation d'un livre :
+exports.rateBook = (req, res, next) => {
+    const userId = req.body.userId;
+    const rating = req.body.rating;
+    if (rating < 0 || rating > 5) {
+        return res.status(400).json({ message: 'La note doit être comprise entre 0 et 5.' });
+    }
+    Book.findOne({ _id: req.params.id })
+        .then((book) => {
+            if (!book) {
+                return res.status(404).json({ message: 'Livre non trouvé.' });
+            }
+            const userRating = book.ratings.find((r) => r.userId === userId);
+            if (userRating) {
+                return res.status(400).json({ message: 'Vous avez déjà noté ce livre.' });
+            }
+            book.ratings.push({ userId: userId, grade: rating });
+            const nbRatings = book.ratings.length;
+            const sumRatings = book.ratings.reduce((acc, r) => acc + r.grade, 0);
+            book.averageRating = sumRatings / nbRatings;
+            book.save()
+                .then(() => res.status(201).json(book))
+                .catch((error) => res.status(400).json({ error }));
+        })
+        .catch((error) => res.status(500).json({ error }));
+};
+
+
+// Calcul de la moyenne d'un livre :
+exports.updateBookRating = (req, res, next) => {
+    const userId = req.auth.userId;
+    const bookId = req.params.id;
+    const grade = req.body.grade;
+
+    // Vérifier si l'utilisateur a déjà noté le livre
+    Book.findOne({ _id: bookId, 'ratings.userId': userId })
+        .then(book => {
+            // Si l'utilisateur a déjà noté le livre, mettre à jour la note existante
+            if (book) {
+                book.ratings.forEach(rating => {
+                    if (rating.userId === userId) {
+                        rating.grade = grade;
+                    }
+                });
+            } else {
+                // Sinon, ajouter une nouvelle note pour l'utilisateur
+                book = new Book({
+                    _id: bookId,
+                    ratings: [{ userId, grade }]
+                });
+            }
+
+            // Calculer la moyenne des notes pour le livre
+            let totalGrade = 0;
+            book.ratings.forEach(rating => {
+                totalGrade += rating.grade;
+            });
+            book.averageRating = totalGrade / book.ratings.length;
+
+            // Sauvegarder les modifications et renvoyer le livre mis à jour
+            book.save()
+                .then(() => res.status(200).json(book))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(400).json({ error }));
+};
+
+// Avoir les 3 livres les mieux notés de la BDD :
+exports.getThreeBestRatings = (req, res, next) => {
+
+    // Récupérer les trois meilleurs livres selon leur note moyenne
+    Book.find()
+        .sort({ averageRating: -1 })
+        .limit(3)
+        .then(bestBooks => {
+            // Renvoyer les trois meilleurs livres
+            res.status(200).json({ bestBooks });
+        })
+        .catch(error => res.status(400).json({ error }));
+};
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -7,9 +7,12 @@ const { upload, processImage } = require('../middleware/multer-config');
 const bookCtrl = require('../controllers/book');
 
 router.get('/',  bookCtrl.getAllBook);
+router.get('/bestrating', bookCtrl.getThreeBestRatings);
 router.get('/:id', bookCtrl.getOneBook);
 router.post('/', auth, upload, processImage, bookCtrl.createBook);
+router.post('/:id/rating', auth, bookCtrl.rateBook);
 router.put('/:id', auth, upload, processImage, bookCtrl.modifyBook);
+router.put('/:id/rating', auth, bookCtrl.updateBookRating);
 router.delete('/:id', auth, bookCtrl.deleteBook);
 
 module.exports = router;
